3f8aa9ba1d13dbe39af61e0e0928bf2365dcc9d8
Jason <mingliang37@163.com>
2018-06-06 20:56:28 +0800

    add new chapter

---
+++ chapter_2/src/exercise/exercise.2.1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <limits.h>
+#include "s_mem.h"
+#include "s_text.h"
+#include "s_type.h"
+
+#include "exercise.2.1.h"
+
+
+int exercise_2_1(void)
+{
+    print_types();
+    
+	return 0;
+}
+
+
---
+++ chapter_2/src/exercise/exercise.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "s_defines.h"
+#include "s_text.h"
+#include "s_cmd.h"
+#include "s_log.h"
+#include "main_proc.h"
+
+#include "exercise.h"
+#include "exercise.2.1.h"
+
+
+#define SUBCMD_EXERCISE "exercise"
+#define SUBCMD_EXERCISE_OPTION_N "-n"
+#define SUBCMD_EXERCISE_OPTION_L "-l"
+
+STRU_MAIN_PROC exercise_proc_array[] =
+{
+    {"2.1", exercise_2_1, "print type size and extremum "},
+
+};
+
+FUNC_MAIN_PROC get_exercise_handler_by_name(const char *name)
+{
+    R_ASSERT(name != NULL, NULL);
+
+    for(int i = 0; i < SIZE_OF_ARRAY(exercise_proc_array); i++)
+    {
+        if(strcmp(exercise_proc_array[i].n_value, name) == 0)
+        {
+            return exercise_proc_array[i].handler;
+        }
+    }
+
+    return NULL;
+}
+
+ENUM_RETURN subcmd_exercise_proc(STRU_OPTION_RUN_BLOCK *value)
+{
+    return RETURN_SUCCESS;
+}
+
+ENUM_RETURN subcmd_exercise_option_n_proc(struct TAG_STRU_ARG *value)
+{
+    R_ASSERT(value != NULL, RETURN_FAILURE);
+    R_ASSERT(value->value != NULL, RETURN_FAILURE);
+
+    FUNC_MAIN_PROC handler = get_exercise_handler_by_name(value->value);
+    if(handler == NULL)
+    {
+        add_current_user_error(ERROR_CODE_INVALID_ARGS, value->value);
+        return RETURN_FAILURE;
+    }
+
+    return handler();;
+}
+
+ENUM_RETURN subcmd_exercise_option_l_proc(struct TAG_STRU_ARG *value)
+{
+    R_ASSERT(value != NULL, RETURN_FAILURE);
+    R_ASSERT(value->value != NULL, RETURN_FAILURE);
+
+    printf("Here is the exercise list:\n");
+    
+    for(int i = 0; i < SIZE_OF_ARRAY(exercise_proc_array); i++)
+    {
+        printf(" %-20s    %s\n", exercise_proc_array[i].n_value, exercise_proc_array[i].introduction);
+
+    }
+
+    printf("\n");
+    
+    return RETURN_SUCCESS;
+}
+
+int exercise_init(void)
+{
+
+    ENUM_RETURN ret_val = RETURN_SUCCESS;
+
+    ret_val = register_subcmd(SUBCMD_EXERCISE, 
+        subcmd_exercise_proc, 
+        "execute exercises.");
+    R_ASSERT(ret_val == RETURN_SUCCESS, RETURN_FAILURE);
+
+    ret_val = register_option(SUBCMD_EXERCISE, 
+        SUBCMD_EXERCISE_OPTION_L, 
+        BOOLEAN_FALSE, 
+        OPTION_TYPE_OPTIONAL, 
+        ARG_TYPE_SWITCH, 
+        subcmd_exercise_option_l_proc, 
+        BOOLEAN_TRUE,
+        "list exercise by name");
+    R_ASSERT(ret_val == RETURN_SUCCESS, RETURN_FAILURE);
+
+    ret_val = register_option(SUBCMD_EXERCISE, 
+        SUBCMD_EXERCISE_OPTION_N, 
+        BOOLEAN_FALSE, 
+        OPTION_TYPE_MANDATORY, 
+        ARG_TYPE_DATA, 
+        subcmd_exercise_option_n_proc, 
+        BOOLEAN_TRUE,
+        "excute exercise by name");
+    R_ASSERT(ret_val == RETURN_SUCCESS, RETURN_FAILURE);
+
+    return RETURN_SUCCESS;
+}
+
+
+
