af5ffa10a0de6a503cf0625da916845a0fb1a1ed
Jason <mingliang37@163.com>
2018-06-17 18:16:27 +0800

    update

--- chapter_3/test/src/example/TestCases.cpp
+++ chapter_3/test/src/example/TestCases.cpp
@@ -1,7 +1,7 @@
 #include "CppUTest/TestHarness.h"
 #include <string.h>
 #include "s_text.h"
-
+#include "s_type.h"
 #include <iostream>
 
 using namespace std;
@@ -23,19 +23,19 @@ TEST_GROUP(TEXT)
 
 TEST(TEXT, test0)
 {
-    char s1[] = "abcd 123";
-    char s2[] = "cd 1";
-    char s[] = "ab23";
+    _S8 s1[] = "abcd 123";
+    _S8 s2[] = "cd 1";
+    _S8 s[] = "ab23";
     squeeze(s1, s2);
     CHECK_EQUAL(strcmp(s1, s), 0);
 }
 
 TEST(TEXT, test1)
 {
-    char s1[] = "abcd 123";
-    char s2[] = "cd 1";
-    char s[] = "cd 123";
-    char *p = any(s1, s2);
+    _S8 s1[] = "abcd 123";
+    _S8 s2[] = "cd 1";
+    _S8 s[] = "cd 123";
+    _S8 *p = any(s1, s2);
     CHECK_EQUAL(strcmp(p, s), 0);
 }
 
