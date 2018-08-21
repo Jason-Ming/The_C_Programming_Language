5fbb1a43e2fce94aa97944dbbc45222cc31db370
Jason-Ming <mingliang37@163.com>
2018-08-21 23:09:36 +0800

    update test files

--- chapter_6/test/src/example/TestCases.cpp
+++ chapter_6/test/src/example/TestCases.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "s_type.h"
 #include "s_mem.h"
+#include "s_print.h"
 #include "s_cproc.h"
 #include <iostream>
 
@@ -25,15 +26,15 @@ TEST_GROUP(S_CC)
 
 TEST(S_CC, test0)
 {
-    _S8 *file_name[] = {
+    const _S8 *file_name[] = {
         //"test_files/func.input",
         //"test_files/c.input",
         //"test_files/s.input",
-        "test_files/empty.input"};
+        "test_files/a.input"};
         //,
         //"test_files/s.input"};
     ENUM_RETURN ret_val;
-    for(_S32 i = 0; i < SIZE_OF_ARRAY(file_name); i++)
+    for(size_t i = 0; i < SIZE_OF_ARRAY(file_name); i++)
     {
         ret_val = s_cc(file_name[i], NULL);
         CHECK_EQUAL(RETURN_SUCCESS, ret_val);
