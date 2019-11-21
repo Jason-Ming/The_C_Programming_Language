b7c86a5e6f398dfd27bf4058624e142a76d95966
Jason-Ming <mingliang37@163.com>
2019-11-21 15:18:30 +0800

    add some standard header file in source files

--- chapter_1/src/exercise/exercise.1.12.c
+++ chapter_1/src/exercise/exercise.1.12.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "s_text.h"
 #include "s_log.h"
--- chapter_3/src/exercise/exercise.3.4.c
+++ chapter_3/src/exercise/exercise.3.4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <limits.h>
 
 #include "s_alg.h"
 
